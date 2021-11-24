f92f66e334f2dd89b7bcb0d9020bd4da6b304b46
Jakub Zochowski <jakub.zochowski997@gmail.com>
2021-11-24 01:20:35 +0000

    Add comments to databse.sql for readability

--- task-3/data-access/queries.sql
+++ task-3/data-access/queries.sql
@@ -1,3 +1,4 @@
+-- Create users table
 CREATE TABLE users 
 	(id serial NOT NULL PRIMARY KEY, 
 	login VARCHAR(255),
@@ -6,11 +7,13 @@ CREATE TABLE users
 	is_disabled BOOLEAN DEFAULT FALSE
 	)
 
+-- Seed users table
 INSERT INTO users (login, password, age) VALUES ('testUser1', 'myPassword1', 21)
 INSERT INTO users (login, password, age) VALUES ('testUser2', 'myPassword2', 31)
 INSERT INTO users (login, password, age) VALUES ('testUser3', 'myPassword3', 41)
 INSERT INTO users (login, password, age) VALUES ('testUser4', 'myPassword4', 51)
 
+-- Create groups table
 CREATE TABLE groups
 	(id serial NOT NULL PRIMARY KEY, 
 	name VARCHAR(255),
@@ -19,15 +22,16 @@ CREATE TABLE groups
         chk_Permissions 
         CHECK(permissions <@ (ARRAY['READ', 'WRITE', 'DELETE', 'SHARE', 'UPLOAD_FILES']))
 	)
-
+-- Seed groups table
 		INSERT INTO groups (name, permissions)
 VALUES ('group2', '{"READ", "WRITE", "DELETE", "SHARE", "UPLOAD_FILES"}');
 
-
-CREATE TABLE user_group (
+-- Create user_group table
+CREATE TABLE user_groups (
   id serial NOT NULL PRIMARY KEY,
-  group_id    int REFERENCES groups (id) ON DELETE CASCADE,
+  group_id int REFERENCES groups (id) ON DELETE CASCADE,
 	user_id int REFERENCES users (id) ON DELETE CASCADE
 );
 
-insert into user_group (user_id, group_id) values (4, 3)
+-- Seed user_groups
+insert into user_groups (user_id, group_id) values (4, 3)
